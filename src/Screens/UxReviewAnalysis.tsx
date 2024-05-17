c33cb7316e650412c3828521fa938ce7812ff05a
ayodeji.olalekan <ayodeji.olalekan@konga.com>
2024-05-17 12:50:33 +0100

    update

--- src/Screens/UxReviewAnalysis.tsx
+++ src/Screens/UxReviewAnalysis.tsx
@@ -110,16 +110,16 @@ export const UxReviewAnalysis = () => {
     return () => clearTimeout(timer);
   }, [dispatch, header]);
 
-  // useEffect(() => {
-  //   const handleScrollToTop = () => {
-  //     window.scrollTo({
-  //       top: 0,
-  //       behavior: "smooth", // Optional: smooth scrolling animation
-  //     });
-  //   };
+  useEffect(() => {
+    const handleScrollToTop = () => {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth", // Optional: smooth scrolling animation
+      });
+    };
 
-  //   handleScrollToTop();
-  // }, []);
+    handleScrollToTop();
+  }, []);
 
   useEffect(() => {
     dispatch(setLastRoute(currentRoute));
