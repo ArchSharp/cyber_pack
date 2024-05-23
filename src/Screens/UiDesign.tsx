5b8755f87be06be9fbabd49e01e8a71aaa058f8e
ayodeji.olalekan <ayodeji.olalekan@konga.com>
2024-05-23 19:48:12 +0100

    update

--- src/Screens/UiDesign.tsx
+++ src/Screens/UiDesign.tsx
@@ -112,16 +112,16 @@ export const UiDesign = () => {
     return () => clearTimeout(timer);
   }, [dispatch, header]);
 
-  //   useEffect(() => {
-  //     const handleScrollToTop = () => {
-  //       window.scrollTo({
-  //         top: 0,
-  //         behavior: "smooth", // Optional: smooth scrolling animation
-  //       });
-  //     };
+  useEffect(() => {
+    const handleScrollToTop = () => {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth", // Optional: smooth scrolling animation
+      });
+    };
 
-  //     handleScrollToTop();
-  //   }, []);
+    handleScrollToTop();
+  }, []);
 
   useEffect(() => {
     dispatch(setLastRoute(currentRoute));
